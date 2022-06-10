7d246a95afcc32bae4b54166f5b0300b27c4d601
Zane Bliss <zane.bliss@daveramsey.com>
2022-06-10 14:10:07 -0500

    Chapter 2

--- spec/money_example_spec.rb
+++ spec/money_example_spec.rb
@@ -6,14 +6,16 @@ class Dollar
   end
 
   def times(multiplier)
-    @amount *= multiplier
+    self.class.new(amount * multiplier)
   end
 end
 
 RSpec.describe "Money Example" do
   it "multiplies" do
     five = Dollar.new(5)
-    five.times(2)
-    expect(10).to be(five.amount)
+    product = five.times(2)
+    expect(product.amount).to be(10)
+    product = five.times(3)
+    expect(product.amount).to be(15)
   end
 end
