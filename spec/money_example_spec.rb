4a3b622d257a6d5f142d1c4c9124b5ad6461df95
Zane Bliss <zane.bliss@daveramsey.com>
2022-06-13 13:15:19 -0500

    Chapter 5

--- spec/money_example_spec.rb
+++ spec/money_example_spec.rb
@@ -16,6 +16,24 @@ class Dollar
   alias_method :equals, :==
 end
 
+class Franc
+  attr_reader :amount
+
+  def initialize(amount)
+    @amount = amount
+  end
+
+  def times(multiplier)
+    self.class.new(amount * multiplier)
+  end
+
+  def ==(dollar)
+    amount == dollar.amount
+  end
+
+  alias_method :equals, :==
+end
+
 RSpec.describe Dollar do
   it "multiplies" do
     five = Dollar.new(5)
@@ -26,5 +44,13 @@ RSpec.describe Dollar do
   it "tests equality" do
     expect(Dollar.new(5).equals(Dollar.new(5))).to be_truthy
     expect(Dollar.new(5).equals(Dollar.new(6))).to be_falsey
+  end
+end
+
+RSpec.describe Franc do
+  it "multiplies" do
+    five = Dollar.new(5)
+    expect(five.times(2)).to eq(Dollar.new(10))
+    expect(five.times(3)).to eq(Dollar.new(15))
   end
 end
